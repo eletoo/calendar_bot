04f36475a677dd2084cec9dea2edfa4e3f4de394
eletoo <e.tonini002@studenti.unibs.it>
2022-10-11 14:47:52 +0200

    tests

--- calendarbot/__main__.py
+++ calendarbot/__main__.py
@@ -1,14 +1,55 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler
+import hashlib
+import logging
 import os
+import random
+import sys
+
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger()
+
+# Getting mode, so we could define run function for local and Heroku setup
+PORT = os.getenv("PORT")
+TOKEN = os.getenv("TG_TOKEN")
 
 
-async def hello(update: Update, context) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+def run_local(updater):
+    updater.start_polling()
+
+
+def run_prod(updater):
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
+    path = hashlib.md5(TOKEN.encode("utf-8")).hexdigest()
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=path)
+    updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, path))
+
+
+def start_handler(update: Update, context: CallbackContext):
+    # Creating a handler-function for /start command
+    logger.info("User {} started bot".format(update.effective_user.id))
+    update.message.reply_text(f"Hello from Python, {update.effective_user.name}!\nPress /random to get random number")
+
+
+def random_handler(update: Update, context: CallbackContext):
+    # Creating a handler-function for /random command
+    number = random.randint(0, 10)
+    logger.info("User {} randomed number {}".format(update.effective_user.id, number))
+    update.message.reply_text("Random number: {}".format(number))
+
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token(os.environ.get('TG_TOKEN')).build()
-    app.add_handler(CommandHandler("hello", hello))
-    app.run_polling()
+    logger.info("Starting bot")
+    updater = Updater(TOKEN)
 
+    updater.dispatcher.add_handler(CommandHandler("start", start_handler))
+    updater.dispatcher.add_handler(CommandHandler("random", random_handler))
 
+    if "HEROKU" in list(os.environ.keys()):
+        run_prod(updater)
+    else:
+        run_local(updater)
