c2f3fc6e85cd40613be2e10da6cf2038d2b28a6a
eletoo <e.tonini002@studenti.unibs.it>
2022-10-18 18:43:27 +0200

    edit

--- calendarbot/__main__.py
+++ calendarbot/__main__.py
@@ -4,15 +4,15 @@ import os
 import random
 import sys
 
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters, CallbackQueryHandler
 
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger()
 
 # Getting mode, so we could define run function for local and Heroku setup
-PORT = int(os.environ.get("PORT"))
+PORT = os.environ.get("PORT")
 TOKEN = os.getenv("TG_TOKEN")
 
 
@@ -27,31 +27,120 @@ def run_prod(updater):
                           port=PORT,
                           url_path=path)
     updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, path))
+    # updater.message.reply_text(main_menu_message(), reply_markup=main_menu_keyboard())
+
+
+###########################################################################################################
+def main_menu(update, context):
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(
+        text=main_menu_message(),
+        reply_markup=main_menu_keyboard())
+
+
+def first_menu(update, context):
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(
+        text=add_event_menu_message(),
+        reply_markup=add_event_menu_keyboard())
+
+
+def second_menu(update, context):
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(
+        text=view_events_menu_message(),
+        reply_markup=view_events_menu_keyboard())
+    update.message.reply_text(query.data)
+
+
+# and so on for every callback_data option
+def first_submenu(bot, update):
+    # query = bot.callback_query
+    pass
+
+
+def second_submenu(bot, update):
+    pass
+
+
+############################ Keyboards #########################################
+def main_menu_keyboard():
+    keyboard = [[KeyboardButton('Aggiungi evento')],
+                [KeyboardButton('Visualizza eventi')]]
+    return keyboard
+
+
+def add_event_menu_keyboard():
+    keyboard = [[KeyboardButton('Gennaio')],
+                [KeyboardButton('Febbraio')],
+                [KeyboardButton('<<Indietro')]]
+    return keyboard
+
+
+def view_events_menu_keyboard():
+    keyboard = [[KeyboardButton('Oggi')],
+                [KeyboardButton('Questa settimana')],
+                [KeyboardButton('Questo mese')],
+                [KeyboardButton('<<Indietro')]]
+    return keyboard
+
+
+############################# Messages #########################################
+def main_menu_message():
+    return 'Seleziona un\'opzione nel menu principale:'
+
+
+def add_event_menu_message():
+    return 'In che mese vuoi aggiungere l\'evento?'
+
+
+def view_events_menu_message():
+    return 'Quali eventi vuoi visualizzare?'
 
 
 def start_handler(update: Update, context: CallbackContext):
     # Creating a handler-function for /start command
     logger.info("User {} started bot".format(update.effective_user.id))
-    update.message.reply_text(f"Hello from Python, {update.effective_user.name}!\nPress /random to get random number")
+    update.message.reply_text(
+        "Ciao, {}!\nPremi /menu per visualizzare le opzioni".format(update.effective_user.name))
 
 
-def random_handler(update: Update, context: CallbackContext):
-    # Creating a handler-function for /random command
-    number = random.randint(0, 10)
+def main_menu(update: Update, context):
+    logger.info("User {} started bot".format(update.effective_user.id))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=main_menu_message(),
+                             reply_markup=ReplyKeyboardMarkup(main_menu_keyboard()))
+
+
+def add_event_menu(update: Update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=add_event_menu_message(),
+                             reply_markup=ReplyKeyboardMarkup(add_event_menu_keyboard()))
+
+
+def view_events_menu(update: Update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=view_events_menu_message(),
+                             reply_markup=ReplyKeyboardMarkup(view_events_menu_keyboard()))
 
 
 def message_handler(update: Update, context: CallbackContext):
     # Creating a handler-function for all messages
     logger.info("User {} sent message: {}".format(update.effective_user.id, update.message.text))
-    update.message.reply_text("You sent me: {}".format(update.message.text))
+    if update.message.text == "Aggiungi evento":
+        add_event_menu(update, context)  # todo: gestire la selezione del mese effettuata dall'utente
+    if update.message.text == "Visualizza eventi":
+        view_events_menu(update, context)  # todo: gestire la visualizzazione degli eventi
 
 
 if __name__ == '__main__':
     logger.info("Starting bot")
     updater = Updater(TOKEN)
 
-    updater.dispatcher.add_handler(CommandHandler("start", start_handler))
-    updater.dispatcher.add_handler(CommandHandler("addevent", random_handler))
+    updater.dispatcher.add_handler(CommandHandler('start', start_handler))
+    updater.dispatcher.add_handler(CommandHandler('menu', main_menu))
+    updater.dispatcher.add_handler(CommandHandler('add_event', add_event_menu))
+    updater.dispatcher.add_handler(CommandHandler('view_events', view_events_menu))
     updater.dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
 
     if "HEROKU" in list(os.environ.keys()):
